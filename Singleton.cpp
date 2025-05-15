0720932653a12c565dc1d7bcc6054bd56152e506
bilaizi <2512075115@qq.com>
2025-05-15 14:09:44 +0800

    Update Singleton.cpp

--- Singleton.cpp
+++ Singleton.cpp
@@ -69,3 +69,26 @@ int main() {
     std::cout << settings.getSetting("godOfTheUnderworld") << std::endl;
     return 0;
 }
+//
+template <class T>
+class SingletonBase {
+protected:
+    SingletonBase() {}
+public:
+    SingletonBase(SingletonBase const &) = delete;
+    SingletonBase& operator=(SingletonBase const&) = delete;
+
+    static T& instance()
+    {
+         static T single;
+         return single;
+    }
+};
+
+class Single : public SingletonBase<Single>
+{
+    Single() {}
+    friend class SingletonBase<Single>;
+public:
+    void demo() { std::cout << "demo" << '\n'; }
+};
